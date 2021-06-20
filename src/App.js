baca7b7ea710c934a4fc1b2950107279ea3b8894
Zaina-coder <81202142+Zaina-coder@users.noreply.github.com>
2021-06-20 16:55:22 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>
+        Weather app
+      </h1>
      <p> This project is coded by zainab 
        <a href="https://github.com/Zaina-coder/newapp" target="_blank">open sourced</a>
      </p>
