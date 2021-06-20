0753b6aaf6d7c3ff03f0c39ed96a7dd8e489c013
Zaina-coder <81202142+Zaina-coder@users.noreply.github.com>
2021-06-19 17:08:59 -0700

    added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <p> This project is coded by zainab 
+       <a href="https://github.com/Zaina-coder/newapp" target="_blank">open sourced</a>
+     </p>
     </div>
   );
 }
